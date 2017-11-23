556fdc517e60317807ce02b62c5d5c4e42c01fb0
Angelb1ue <szez0037@163.com>
2017-11-23 20:59:14 +0800

    add photos

--- master/tool.py
+++ master/tool.py
@@ -90,6 +90,11 @@ def compress_photo():
     compress('4', des_dir, src_dir, file_list_src)
 
 def handle_photo():
+    '''根据图片的文件名处理成需要的json格式的数据
+    
+    -----------
+    最后将data.json文件存到博客的source/photos文件夹下
+    '''
     src_dir, des_dir = "photos/", "min_photos/"
     file_list = list_img_file(src_dir)
     list_info = []
@@ -166,7 +171,7 @@ if __name__ == "__main__":
     cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
     compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
     git_operation()    # 提交到github仓库
-    handle_photo()    
+    handle_photo()     # 将文件处理成json格式，存到博客仓库中
     
     
     
